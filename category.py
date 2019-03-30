df7f9acbcdc93f7e89a06676c9be1eea94c2434e
Asim Sansi <general_iroh@whitelotus.peace>
2019-03-30 19:57:52 +0500

    category class is created

---
+++ category.py
@@ -0,0 +1,7 @@
+
+import numpy as np
+class _category:
+    def __init__(self, name, items):
+        self.name = name;
+        self.items = items
+
--- db_handling.py
+++ db_handling.py
@@ -5,8 +5,8 @@ import pymysql
 # Connect to the database
 connection = pymysql.connect(host='localhost',
                              user='root',
-                             password='password',
-                             db='testing',
+                             password='tipu',
+                             db='logault_database',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
@@ -15,7 +15,7 @@ try:
         # Read a single record
         sql = "SELECT `*` FROM `label`"
         cursor.execute(sql)
-        result = cursor.fetchall()
+        result = cursor.fetchone()
         for i in result:
            print(i["tag_name"])
 finally:
